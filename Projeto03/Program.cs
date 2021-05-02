a178eec69c78f54e5db0b2fbb63580caf3d23275
NatySousa <nataliaspsousa@gmail.com>
2021-05-02 13:06:34 -0300

    versão inicial do projeto

---
+++ Projeto03/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Projeto03.Entities;
+using System.Collections.Generic;
+using Projeto03.Repositories;
+
+namespace Projeto03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nCONTROLE DE FORNECEDORES E PRODUTOS\n");
+
+            var fornecedor = new Fornecedor();
+            fornecedor.Produtos = new List<Produto>();
+
+            fornecedor.Id = Guid.NewGuid();
+
+            Console.WriteLine("Informe o nome do fornecedor:");
+            fornecedor.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o cnpj do fornecedor:");
+            fornecedor.Cnpj = Console.ReadLine();
+
+            Console.WriteLine("Informe a quantidade de produtos:");
+            var qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine("\nAdicione o produto do fornecedor:\n");
+              
+                var produto = new Produto();
+                produto.Id = Guid.NewGuid();
+
+                Console.WriteLine("Informe o nome do produto: ");
+                produto.Nome = Console.ReadLine();
+
+                Console.WriteLine("Informe o Preco do produto: ");
+                produto.Preco = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe a quantidade do produto: ");
+                produto.Quantidade = int.Parse(Console.ReadLine());
+
+                fornecedor.Produtos.Add(produto);
+
+                Console.WriteLine("\nProduto adicionado no fornecedor com sucesso.");
+
+            }
+
+            try
+            {
+                var fornecedorRepository = new FornecedorRepository();
+
+                fornecedorRepository.ExportarTxt(fornecedor);
+                fornecedorRepository.ExportarXml(fornecedor);
+                fornecedorRepository.ExportarJson(fornecedor);
+ 
+
+
+                Console.WriteLine("\nDados gravados com sucesso! ");
+
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("\nOcorreu um erro: " + e.Message);
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Projeto03/Repositories/FornecedorRepository.cs
@@ -0,0 +1,63 @@
+﻿using Projeto03.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Projeto03.Repositories
+{
+   public class FornecedorRepository
+    {
+        public void ExportarTxt(Fornecedor fornecedor)
+        {
+            var path = $"c:\\temp\\fornecedor_{fornecedor.Id }.txt";
+
+            using (var streamWriter = new StreamWriter(path)) 
+            {
+                streamWriter.WriteLine("Id do fornecedor: " + fornecedor.Id);
+                streamWriter.WriteLine("Nome do fornecedor: " + fornecedor.Nome);
+                streamWriter.WriteLine("Cnpj do fornecedor: " + fornecedor.Cnpj);
+
+                foreach (var item in fornecedor.Produtos)
+
+                {
+                    streamWriter.WriteLine("\nProduto:");
+                    streamWriter.WriteLine("Id do Produto: " + item.Id);
+                    streamWriter.WriteLine("Nome do Produto: " + item.Nome);
+                    streamWriter.WriteLine("Preço do Produto: " + item.Preco);
+                    streamWriter.WriteLine("Quantidade do Produto: " + item.Quantidade);
+
+                }
+            }
+        }
+
+        public void ExportarXml(Fornecedor fornecedor) 
+        {
+            var path = $"c:\\temp\\fornecedor_{fornecedor.Id}.xml";
+
+            var xmlSerializer = new XmlSerializer(fornecedor.GetType());
+
+            using (var streamWriter = new StreamWriter(path)) 
+            {
+                xmlSerializer.Serialize(streamWriter, fornecedor);
+            }
+        }
+
+        public void ExportarJson(Fornecedor fornecedor)
+        {
+            var path = $"c:\\temp\\fornecedor_{fornecedor.Id}.json";
+
+            var json = JsonConvert.SerializeObject(fornecedor, Formatting.Indented);
+
+            using (var streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine(json);
+            }
+
+        }
+
+        
+    }
+}
